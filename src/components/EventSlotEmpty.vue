7919d017983b357187544ae1f906abbd95d7360f
Mike Ciccone <mc7263@gmail.com>
2022-06-21 14:33:46 -0700

    working with headless.ui

---
+++ src/components/EventSlotEmpty.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="event-block w-32 h-16"></div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        'table': '2560px',
+        '128': '32rem'
+      }
+    },
+  },
+  plugins: [],
+}
